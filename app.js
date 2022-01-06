af8ada1ba95d95f232d458e57e0d31e740140587
naAlmanzor <neilalmanzor28@gmail.com>
2022-01-06 12:24:47 +0800

    Updated html and js files
    -now calculates inputs from input.html
    -calculations are passed and displayed to calorie-tracking.html

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-function calc(){
-    // document.getElementById('age-display') = document.getElementById('ageInput')
+function calculate(){
+    var bmr = 0;
+    var calories = 0;
+    var ageData = document.getElementById('ageVal').value;
+    var genderData = document.querySelector('input[name="gender"]:checked').value;
+    var heightData = document.getElementById('heightVal').value;
+    var weightData = document.getElementById('weightVal').value;
+    var option = document.getElementById("activitySelect").value;
+    
+    if(genderData == 'Male'){
+        bmr = (10*weightData)+(6.25*heightData) - (5*ageData) + 5
+        // bmr = (10*65)+(6.25*180) - (5*15) + 5;
+    }
 
-    console.log('Hello')
+    if(genderData == 'Female'){
+        bmr = (10*weightData)+(6.25*heightData) - (5*ageData) - 161
+        // bmr = (10*65)+(6.25*180) - (5*15) - 161;
+    }
+
+    if (option=="Sedentary"){
+        calories = bmr*1.2
+    }
+    if (option=="Light"){
+        calories = bmr*1.375
+    }
+    if (option=="Moderate"){
+        calories = bmr*1.55
+    }
+    if (option=="Very"){
+        calories = bmr*1.725
+    }
+
+    sessionStorage.setItem("bmr", bmr)
+    sessionStorage.setItem("calories", calories);
+
+    alert(bmr+" "+calories);
 }
+
+document.getElementById('displayBMR').innerHTML = sessionStorage.getItem("bmr");
+document.getElementById('displayCalories').innerHTML = sessionStorage.getItem("calories");
+window.onreset = sessionStorage.clear();
