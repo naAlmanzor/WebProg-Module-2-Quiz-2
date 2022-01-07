db4c48654e18bc792d0ba581b9aeda219fe7ee20
naAlmanzor <neilalmanzor28@gmail.com>
2022-01-07 09:41:03 +0800

    Added alert for unfilled inputs, updated design

--- app.js
+++ app.js
@@ -5,40 +5,51 @@ function calculate(){
     var genderData = document.querySelector('input[name="gender"]:checked').value;
     var heightData = document.getElementById('heightVal').value;
     var weightData = document.getElementById('weightVal').value;
+    var foodName = document.getElementById("foodName").value;
     var foodData = document.getElementById('foodCalories').value;
 
     var option = document.getElementById("activitySelect").value;
+
+    if (!ageData || !heightData || !weightData ||!foodName ||!foodData){
+        alert("Error! Form must be filled up.");
+    }    
+
+    else{
+
+        if(genderData == 'Male'){
+            bmr = (10*weightData)+(6.25*heightData) - (5*ageData) + 5
+            // bmr = (10*65)+(6.25*180) - (5*15) + 5;
+        }
     
-    if(genderData == 'Male'){
-        // bmr = (10*weightData)+(6.25*heightData) - (5*ageData) + 5
-        bmr = (10*65)+(6.25*180) - (5*15) + 5;
-    }
+        if(genderData == 'Female'){
+            bmr = (10*weightData)+(6.25*heightData) - (5*ageData) - 161
+            // bmr = (10*65)+(6.25*180) - (5*15) - 161;
+        }    
+ 
+        if (option=="BMR"){
+            calories = bmr;
+        }
+    
+        if (option=="Sedentary"){
+            calories = bmr*1.2;
+        }
+        if (option=="Light"){
+            calories = bmr*1.375;
+        }
+        if (option=="Moderate"){
+            calories = bmr*1.55;
+        }
+        if (option=="Very Active"){
+            calories = bmr*1.725;
+        }
 
-    if(genderData == 'Female'){
-        // bmr = (10*weightData)+(6.25*heightData) - (5*ageData) - 161
-        bmr = (10*65)+(6.25*180) - (5*15) - 161;
+        document.getElementById("caloriesCalc").action = "calorie-tracking.html"
     }
-
-    if (option=="BMR"){
-        calories = bmr;
-    }
-
-    if (option=="Sedentary"){
-        calories = bmr*1.2;
-    }
-    if (option=="Light"){
-        calories = bmr*1.375;
-    }
-    if (option=="Moderate"){
-        calories = bmr*1.55;
-    }
-    if (option=="Very Active"){
-        calories = bmr*1.725;
-    }
+    
 
     sessionStorage.setItem("option",option);
     sessionStorage.setItem("calories", calories+" calories/day");
-    sessionStorage.setItem("foodName", "With "+document.getElementById("foodName").value);
+    sessionStorage.setItem("foodName", "With "+foodName);
     sessionStorage.setItem("foodCalories", (calories-foodData)+" calories/day");
 }
 
